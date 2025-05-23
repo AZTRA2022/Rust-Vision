df7621afc8b4bf9cee7d04ea9efd034fcb52fd64
AZTRA2022 <konanisaac6594@gmail.com>
2025-05-23 17:36:38 +0000

    i was able to separe the code of the functions , eacg in his file the face methods et and the video methods

---
+++ TREE.md
@@ -0,0 +1,29 @@
+# Three of the project
+### supose that you are in the root of the project
+```bash
+    tree
+```
+
+### **Result** 
+```bash
+    "├── Cargo.lock
+├── Cargo.toml
+├── CODE_OF_CONDUCT.md
+├── CONTRIBUTING.md
+├── face.jpeg
+├── LICENSE
+├── README.md
+├── src
+│   ├── face.rs
+│   ├── main.rs
+│   └── video.rs
+└── TREE.md
+"
+```
+
+## Avertissement:
+### This is not a complete tree. And most being considered as a initial three of the project.
+
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use opencv::core::Mat;
+use crate::face::detect_face;
 use crate::video::{CameraSource, Video};
 pub mod video;
 mod face;
@@ -6,16 +6,24 @@ mod face;
 fn main() {
     let mut video = Video::new(
         CameraSource::Online("http://192.168.1.10:4747/video/".to_string()),
-        // use another CameraSource if your computer have a Webcam
+        // use another CameraSource if your setup has a Webcam
         (640.0, 480.0),
         60.0,
     );
-
+    //let mut temp = &video;
+    // Initialiser d'abord la vidéo
     if let Err(e) = video.initialize() {
-        eprintln!("Error initializing video: {}", e);
+        eprintln!("Erreur d'initialisation de la vidéo: {}", e);
         return;
     }
 
+    // Lire d'abord la frame
+    if let Ok(mut frame) = video.read_frame() {
+        // Puis passer video et frame à detect_face
+        if let Err(e) = detect_face(&mut video, &mut frame) {
+            eprintln!("Erreur lors de la détection de visage: {}", e);
+        }
+    }
     if let Err(e) = video.display("Video Capture from Online Source") {
         eprintln!("Error during display: {}", e);
     }
